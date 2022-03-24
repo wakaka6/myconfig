cb29265533169b572975c29a07b6ecfbc3ee5039
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2022-03-24 19:43:01 +0800

    awesome fzf comparate with rg

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -151,3 +151,13 @@ bindkey '^R' fzf-history-widget
   eval $__fzf_key_bindings_options
   'unset' '__fzf_key_bindings_options'
 }
+
+
+
+# Awesome fzf comparate with rg
+#find-in-file - usage: fif <searchTerm>
+fif() {
+  if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+  rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+}
+
