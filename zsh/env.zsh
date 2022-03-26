edd54c000e8fbcc6b2198f58d552b49aabd28f00
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2022-03-26 16:50:02 +0800

    change go space

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,7 @@
 # ===
 # === ENV
 # ===
-export GOPATH="$HOME/Documents/code/gospace"
+export GOPATH="$HOME/code/gospace"
 export PATH=$PATH:$GOPATH/bin
 export EDITOR="nvim"
 export XDG_CONFIG_HOME=$HOME/.config
