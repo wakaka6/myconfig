f772d0508d30a95f5e83555d04edf57624026fec
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2022-08-30 01:37:56 +0800

    drcula theme for fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -14,7 +14,7 @@
 # Key bindings
 # ------------
 
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(highlight -O ansi {} || bat {}) 2> /dev/null | head -500'"
+export FZF_DEFAULT_OPTS="--color=fg:#f8f8f2,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --height 40% --layout=reverse --preview '(highlight -O ansi {} || bat {}) 2> /dev/null | head -500'"
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT='80%'
 
