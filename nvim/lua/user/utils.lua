9fe012f8e2e293345117b9151ae004dbd00f573d
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2022-09-18 11:51:27 +0800

    add auto load lunch.json utils function

--- nvim/lua/user/utils.lua
+++ nvim/lua/user/utils.lua
@@ -1,23 +1,34 @@
 local M = {}
 
 M.SaveAndExit = function()
-  -- wirte all buffer first
-  vim.api.nvim_command(":wa")
-  -- quit all buffer
-  vim.api.nvim_command(":qa")
+    -- wirte all buffer first
+    vim.api.nvim_command(":wa")
+    -- quit all buffer
+    vim.api.nvim_command(":qa")
 end
 
 
 -- file exist?
 M.exists = function(file)
-  local ok, err, code = os.rename(file, file)
-  if not ok then
-    if code == 13 then
-      -- Permission denied, but it exists
-      return true
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
     end
-  end
-  return ok, err
+    return ok, err
+end
+
+M.split = function(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
 end
 
 return M
