ab33a1fb8f54d60f0dd3a37e9f26db526eed6224
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2023-06-30 14:17:34 +0800

    Update translate using proxy to bypass GFW

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,13 +2,14 @@
 # === aliases
 # === 
 alias nv=nvim
+alias ls=lsd
 alias sudo="sudo "
 alias ra="ranger"
 alias lg="lazygit"
 alias ipy="ipython3"
 alias pweb="python -m http.server 8000"
-alias tldr="tldr -t ocean"
+# alias tldr="tldr -t ocean"
 alias pxy="proxychains -q"
-alias transc="trans -t en -shell"
-alias transe="trans -t zh -shell"
-alias transl="trans -t en+zh -shell"
+alias transc="proxychains -q trans -t en -shell"
+alias transe="proxychains -q trans -t zh -shell"
+alias transl="proxychains -q trans -t en+zh -shell"
