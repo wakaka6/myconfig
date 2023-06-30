c00a338559e42edf82cf1e800b8e537e5eab0c95
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2023-06-30 14:18:41 +0800

    Update enviroment

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,9 +6,12 @@ export PATH=$PATH:$GOPATH/bin
 export EDITOR="nvim"
 export XDG_CONFIG_HOME=$HOME/.config
 
+# rust
+export PATH=$PATH:$HOME/.cargo/bin
 
 
 
 
+eval "$(zoxide init --cmd cd zsh)"
 
 
