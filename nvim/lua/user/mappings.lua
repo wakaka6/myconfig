5e76ba1aed7ac526f1b82425d8a04ed05f8359da
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-05-03 13:01:14 +0800

    Updated mapping to lua

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -1,26 +1,26 @@
-vim.g.mapleader = "<Space>"
+vim.g.mapleader = " "
 
 -- unmap ctrl+j as null
 vim.g.BASH_Ctrl_j = "off"
 
-vim.keymap.set("n", "S", ":w<CR>", { noremap = true })
-vim.keymap.set("n", "Q", ":q<CR>", { noremap = true })
+vim.keymap.set("n", "S", "<cmd>w<CR>", { noremap = true })
+vim.keymap.set("n", "Q", "<cmd>q<CR>", { noremap = true })
 
 vim.keymap.set("i", "<C-q>", "<Esc>", { noremap = true })
 vim.keymap.set("x", "<C-q>", "<Esc>", { noremap = true })
 vim.keymap.set("s", "<C-q>", "<Esc>", { noremap = true })
 
 -- Press space twice to jump to the next '<,.>' and edit it.
-vim.keymap.set("n", "<Leader><Leader>", [[<Esc>/<,.><CR>:nohlsearch<CR>"_c4l]], { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader><Leader>", [[<Esc>/<,.><CR><cmd>nohlsearch<CR>"_c4l]], { noremap = true, silent = true })
 vim.keymap.set("n", "<Leader>|", "a<,.><Esc>", { noremap = true, silent = true })
 
 -- toggle buffer
-vim.keymap.set("n", "[b", ":bprevious<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "]b", ":bnext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Leader>[", ":bprevious<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Leader>]", ":bnext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "[B", ":bfirst<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "]B", ":blast<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "[b", "<cmd>bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>[", "<cmd>bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>]", "<cmd>bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "[B", "<cmd>bfirst<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "]B", "<cmd>blast<CR>", { noremap = true, silent = true })
 
 -- change better pane move method
 vim.keymap.set("n", "<Leader>h", "<C-w>h", { noremap = true, silent = true })
@@ -29,10 +29,10 @@ vim.keymap.set("n", "<Leader>k", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<Leader>l", "<C-w>l", { noremap = true, silent = true })
 
 -- resize pane with ALT+arrow
-vim.keymap.set("n", "<M-Up>", ":resize -2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<M-Down>", ":resize +2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<M-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<M-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Up>", "<cmd>resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Down>", "<cmd>resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Left>", "<cmd>vertical resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Right>", "<cmd>vertical resize +2<CR>", { noremap = true, silent = true })
 
 -- Command Mode Cursor Movement
 vim.keymap.set("c", "<C-a>", "<Home>", { noremap = true })
@@ -53,7 +53,7 @@ vim.keymap.set("n", "<", "<<", { noremap = true })
 vim.keymap.set("n", ">", ">>", { noremap = true })
 
 -- search
-vim.keymap.set("n", "<Leader><CR>", ":nohlsearch<CR><C-l>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader><CR>", "<cmd>nohlsearch<CR><C-l>", { noremap = true, silent = true })
 
 -- Copy to system clipboard
 vim.keymap.set("v", "Y", '"+y', { noremap = true })
@@ -62,4 +62,9 @@ vim.keymap.set("v", "Y", '"+y', { noremap = true })
 vim.keymap.set("n", "0", [[col('.') == 1 ? '^' : '0']], { expr = true })
 
 -- 寻找中文
-vim.keymap.set("n", "<Leader>fz", [[<Esc>/\v<[\u4e00-\u9fa5]+>/<CR>:nohlsearch<CR>]], { noremap = true, silent = true })
+vim.keymap.set(
+	"n",
+	"<Leader>fz",
+	[[<Esc>/\v<[\u4e00-\u9fa5]+>/<CR><cmd>nohlsearch<CR>]],
+	{ noremap = true, silent = true }
+)
