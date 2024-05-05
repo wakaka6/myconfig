1cf792dc97b2637ce2287155e3f10cb4e5ef33cb
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-05-05 13:23:15 +0800

    Added code fold

--- nvim/lua/user/conf/lsp/init.lua
+++ nvim/lua/user/conf/lsp/init.lua
@@ -7,6 +7,7 @@ return {
 	require("user.conf.lsp.autocomplete").lua_config,
 	require("user.conf.lsp.lint").null_ls,
 	require("user.conf.lsp.formatting"),
+	require("user.conf.lsp.fold"),
 
 	require("user.conf.lsp.go"),
 }
