2144653145d7bdce67b6c0158b4f4d6ba3018e1f
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-10-24 01:20:29 +0800

    Updated vim colorscheme

--- nvim/lua/user/conf/theme/init.lua
+++ nvim/lua/user/conf/theme/init.lua
@@ -1,31 +1,14 @@
 return {
-    {
-        'wakaka6/dracula.nvim',
-        branch = 'custom',
-        lazy = false,
-        priority = 1100,
-        config = function()
-            vim.cmd('colorscheme dracula')
-            -- show the '~' characters after the end of buffers
-            vim.g.dracula_show_end_of_buffer = true
-            -- use transparent background
-            vim.g.dracula_transparent_bg = false
-            -- set custom lualine background color
-            -- vim.g.dracula_lualine_bg_color = "#44475a"
-            -- set italic comment
-            vim.g.dracula_italic_comment = true
-        end
-    },
-    'ryanoasis/vim-devicons',
-    require("user.conf.theme.lualine"),
-    require("user.conf.theme.bufferline"),
-    require("user.conf.theme.dashboard"),
-    -- {
-    --     'Bekaboo/dropbar.nvim',
-    --     -- optional, but required for fuzzy finder support
-    --     dependencies = {
-    --         'nvim-telescope/telescope-fzf-native.nvim'
-    --     },
-    -- },
-
+	require("user.conf.theme.colorscheme").custom,
+	"ryanoasis/vim-devicons",
+	require("user.conf.theme.lualine"),
+	require("user.conf.theme.bufferline"),
+	require("user.conf.theme.dashboard"),
+	{
+		"Bekaboo/dropbar.nvim",
+		-- optional, but required for fuzzy finder support
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+	},
 }
