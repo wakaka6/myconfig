fca767d50d5a28ba3a204c38043c1f98aaee1eca
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-10-24 21:14:21 +0800

    Updated switch null-ls to none-ls

--- nvim/lua/user/conf/lsp/lint.lua
+++ nvim/lua/user/conf/lsp/lint.lua
@@ -25,7 +25,7 @@ M.nlint = {
 }
 
 M.null_ls = {
-	"jose-elias-alvarez/null-ls.nvim",
+	"nvimtools/none-ls.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local null_ls = require("null-ls")
