6eec416c7d06f1326968f54c088c7443d508e1dd
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-11-09 16:12:52 +0800

    Updated set background to transparent

--- nvim/lua/user/conf/theme/colorscheme.lua
+++ nvim/lua/user/conf/theme/colorscheme.lua
@@ -6,15 +6,15 @@ M.custom = {
 	lazy = false,
 	priority = 1100,
 	config = function()
+		local dracula = require("dracula")
+		dracula.setup({
+			-- show the '~' characters after the end of buffers
+			show_end_of_buffer = true, -- default false
+			-- use transparent background
+			transparent_bg = true,
+			italic_comment = true, -- default false
+		})
 		vim.cmd("colorscheme dracula")
-		-- show the '~' characters after the end of buffers
-		vim.g.dracula_show_end_of_buffer = true
-		-- use transparent background
-		vim.g.dracula_transparent_bg = false
-		-- set custom lualine background color
-		-- vim.g.dracula_lualine_bg_color = "#44475a"
-		-- set italic comment
-		vim.g.dracula_italic_comment = true
 	end,
 }
 
