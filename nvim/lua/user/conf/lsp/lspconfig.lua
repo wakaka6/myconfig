bc8eec9689f8a13f1201ddbb46b718b706df3de8
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-11-13 17:17:36 +0800

    Updated ruff only be use lint and formatter

--- nvim/lua/user/conf/lsp/lspconfig.lua
+++ nvim/lua/user/conf/lsp/lspconfig.lua
@@ -112,6 +112,23 @@ return {
 					},
 				})
 			end,
+			["pyright"] = function()
+				lspconfig["pyright"].setup({
+					capabilities = capabilities,
+					settings = {
+						pyright = {
+							-- Using Ruff's import organizer
+							disableOrganizeImports = true,
+						},
+						python = {
+							analysis = {
+								-- Ignore all files for analysis to exclusively use Ruff for linting
+								ignore = { "*" },
+							},
+						},
+					},
+				})
+			end,
 		})
 	end,
 }
