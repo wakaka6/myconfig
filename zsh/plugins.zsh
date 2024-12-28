a7ab2b01eab28d744a51fbb08fdd04a41fe0a27e
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-12-29 00:52:58 +0800

    Updated use zimfw instead of ohmyzsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,34 +1,7 @@
-# zunplugged: https://github.com/mattmc3/zsh_unplugged
-# a simple, ultra-fast plugin handler
-
-# clone a plugin, identify its init file, source it, and add it to your fpath
-function plugin-load() {
-  local repo plugin_name plugin_dir initfile initfiles
-  ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
-  for repo in $@; do
-    plugin_name=${repo:t}
-    plugin_dir=$ZPLUGINDIR/$plugin_name
-    initfile=$plugin_dir/$plugin_name.plugin.zsh
-    if [[ ! -d $plugin_dir ]]; then
-      echo "Cloning $repo"
-      git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugin_dir
-    fi
-    if [[ ! -e $initfile ]]; then
-      initfiles=($plugin_dir/*.plugin.{z,}sh(N) $plugin_dir/*.{z,}sh{-theme,}(N))
-      [[ ${#initfiles[@]} -gt 0 ]] || { echo >&2 "Plugin has no init file '$repo'." && continue }
-      ln -sf "${initfiles[1]}" "$initfile"
-    fi
-    fpath+=$plugin_dir
-    (( $+functions[zsh-defer] )) && zsh-defer . $initfile || . $initfile
-  done
-}
-
-# if you want to compile your plugins you may see performance gains
-function plugin-compile() {
-  ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
-  autoload -U zrecompile
-  local f
-  for f in $ZPLUGINDIR/**/*.zsh{,-theme}(N); do
-    zrecompile -pq "$f"
-  done
-}
+export ZIM_HOME=${XDG_CACHE_HOME}/zim
+if [[ ! -d $ZIM_HOME ]]; then
+    echo "Installing Zimfw"
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+    rm ~/.config/zsh/.zimrc
+    cp ~/.config/zsh/zimrc ~/.config/zsh/.zimrc
+fi
