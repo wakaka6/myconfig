e73db7b1517e910c7f9bab6af0caa8d75f909b37
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-12-29 01:40:52 +0800

    Updated nvim fomatter for toml

--- nvim/lua/user/conf/lsp/formatting.lua
+++ nvim/lua/user/conf/lsp/formatting.lua
@@ -14,6 +14,7 @@ return {
 				css = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
+				toml = { "taplo" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 				graphql = { "prettier" },
