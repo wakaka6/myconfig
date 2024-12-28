233d25d839f929bd958261b7618b3997de11efe9
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-12-29 01:41:11 +0800

    Updated yazi theme dracula

--- yazi/init.lua
+++ yazi/init.lua
@@ -10,7 +10,7 @@ Header:children_add(function()
 	if ya.target_family() ~= "unix" then
 		return ""
 	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("green")
 end, 500, Header.LEFT)
 
 Status:children_add(function()
