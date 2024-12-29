6c9e450354c03756fd04ba71a52d72aae146bcab
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2024-12-29 17:13:04 +0800

    Updated: use starship

--- yazi/init.lua
+++ yazi/init.lua
@@ -50,14 +50,6 @@ require("yatline"):setup({
 	},
 })
 
--- show current user and host
-Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ""
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("green")
-end, 500, Header.LEFT)
-
 require("git"):setup()
 require("starship"):setup()
 
